3bbe27e1c5674e6c8e1d053f8bf7732c6a13f2cf
Fara Akhatova <faraakhatova@gmail.com>
2021-04-22 16:17:43 -0600

    Add function with event target to find which weapon pkayer is choosing

--- main.js
+++ main.js
@@ -53,3 +53,28 @@ function gameChoices(event) {
 
   }
 }
+
+function humanWeapon(event) {
+  var getWeapon = event.target.id
+  if (getWeapon === 'rock') {
+    game.humanWeapon = 'rock'
+    leftSideWeapon.innerHTML += `<img class='rock-weapon' id='rock' src='https://cdn0.iconfinder.com/data/icons/rock-paper-scissors-emoji/792/rock-paper-scissors-emoji-cartoon-011-512.png'/>`
+  }
+  if (getWeapon === 'paper') {
+    game.humanWeapon = 'paper'
+    leftSideWeapon.innerHTML += `<img class='paper-weapon' id='paper' src='https://cdn0.iconfinder.com/data/icons/rock-paper-scissors-emoji/792/rock-paper-scissors-emoji-cartoon-019-512.png'/>`
+  }
+  if (getWeapon === 'scissors') {
+    game.humanWeapon = 'scissors'
+    leftSideWeapon.innerHTML += `<img class='scissor-weapon' id='scissors' src='https://cdn0.iconfinder.com/data/icons/rock-paper-scissors-emoji/792/rock-paper-scissors-emoji-cartoon-009-512.png'/>`
+  }
+  // if (getWeapon === 'spock') {
+  //   game.humanWeapon = 'spock'
+  //   // gameBoard.innerHtml += `  <img class='spock-weapon' id='spock' src='https://w7.pngwing.com/pngs/365/477/png-transparent-spock-character-snoopy-homo-sapiens-t-shirt-mischief-boy-cartoon-fictional-character.png'/>`
+  // }
+  // if (getWeapon === 'lizard') {
+  //   game.humanWeapon = 'lizard'
+  //   // gameBoard.innerHTML += `<img class='lizard-weapon' id='lizard' src='🦎'/>`
+  // }
+
+}
