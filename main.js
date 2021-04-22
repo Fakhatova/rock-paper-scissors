4664d6165551274e2a860852450caae5bb683e70
Fara Akhatova <faraakhatova@gmail.com>
2021-04-22 14:22:17 -0600

    Add function to hide and show elements, and event listener

--- main.js
+++ main.js
@@ -1,14 +1,30 @@
 // ******** QUERY SELECTORS ******** //
-
 var gameBoard = document.getElementById('gameBoard');
 var gameOption = document.getElementById('gameChoice');
-var classicGame = document.getElementById('chooseGame')
-var additionGame = document.getElementById('chooseGameAddition')
+var classicGame = document.getElementById('chooseGame');
+var additionGame = document.getElementById('chooseGameAddition');
 var humanPlayer = document.getElementById('humanPlay');
-var leftSideWeapon = document.getElementById('left')
-var rightSideWeapon = document.getElementById('right')
+var leftSideWeapon = document.getElementById('left');
+var rightSideWeapon = document.getElementById('right');
 
 
 
 // ******** GLOBAL VARIABLES ******** //
 var game = new Game();
+
+
+// ******** EVENTLISTENERS ******** //
+gameBoard.addEventListener('click', gameOption);
+humanPlayer.addEventListener('click', declareWinner);
+
+
+
+// ******** EVENT HANDLERS AND FUNCTIONS ******** //
+
+function hideShow(element, hidden) {
+  if (hidden) {
+    element.classList.remove('hidden');
+  } else {
+    element.classList.add('hidden');
+  }
+}
