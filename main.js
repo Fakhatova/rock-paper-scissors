ed9a8170dc9b9bbb97ef922cac0da94fae7c6a2e
Fara Akhatova <faraakhatova@gmail.com>
2021-04-22 14:09:52 -0600

    Query html elements

---
+++ main.js
@@ -0,0 +1,14 @@
+// ******** QUERY SELECTORS ******** //
+
+var gameBoard = document.getElementById('gameBoard');
+var gameOption = document.getElementById('gameChoice');
+var classicGame = document.getElementById('chooseGame')
+var additionGame = document.getElementById('chooseGameAddition')
+var humanPlayer = document.getElementById('humanPlay');
+var leftSideWeapon = document.getElementById('left')
+var rightSideWeapon = document.getElementById('right')
+
+
+
+// ******** GLOBAL VARIABLES ******** //
+var game = new Game();
