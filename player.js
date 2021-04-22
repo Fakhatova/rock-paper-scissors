59dfa8e1c46892bf53ef9e59991f797a716e2963
Fara Akhatova <faraakhatova@gmail.com>
2021-04-22 16:09:40 -0600

    Add class name and methods for player class

--- player.js
+++ player.js
@@ -1,3 +1,18 @@
 class Player {
+  constructor(id) {
+    this.id = id;
+    this.wins = 0;
+  }
+
+  saveWinsToStorage() {
+
+  }
+
+  retrieveWinsFromStorage() {
+
+  }
+  takeTurns() {
+
+  }
 
 }
