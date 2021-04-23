c1f7ee8e0712a8df849f96a84a9205331823dd6d
Fara Akhatova <faraakhatova@gmail.com>
2021-04-23 11:08:31 -0600

    Write method take turns to identify which player is taking turns first and second

--- player.js
+++ player.js
@@ -1,6 +1,6 @@
 class Player {
   constructor(name, wins, token) {
-    this.id = Date.now();
+    // this.id = Date.now();
     this.name = null;
     this.wins = 0;
     this.token = null;
@@ -15,7 +15,17 @@ class Player {
 
   }
   takeTurns() {
-
+    if (game.humanTurn === null) {
+      game.humanTurn = 'first'
+      game.human.name = 'Human'
+      game.human.token = '👩🏻‍⚕️'
+      console.log('heyooo')
+    } else {
+      game.coronaTurn = 'second'
+      game.corona.name = 'Corona'
+      game.corona.token = '🦠'
+      console.log('coronas turn')
+    }
   }
 
 }
