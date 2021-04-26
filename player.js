17157efa7265a2a048f83bf2bc2abb9506db9e2c
Fara Akhatova <faraakhatova@gmail.com>
2021-04-26 08:42:28 -0600

    Add refactor into local storage and fix bug

--- player.js
+++ player.js
@@ -5,23 +5,35 @@ class Player {
     this.token = null;
   }
 
-  saveWinsToStorage() {
+  saveHumanWinsToStorage() {
     var human = game.human
-    var corona = game.corona
     localStorage.setItem('human', JSON.stringify(human))
+  }
+
+
+  saveCoronaWinsToStorage() {
+    var corona = game.corona
     localStorage.setItem('corona', JSON.stringify(corona))
   }
 
   retrieveHumanWinsFromStorage() {
     var humanWins = localStorage.getItem('human');
     humanWins = JSON.parse(humanWins)
-    return humanWins.wins;
+    if (humanWins) {
+      return humanWins.wins;
+    } else {
+      return 0;
+    }
   }
 
   retrieveCoronaWinsFromStorage() {
     var coronaWins = localStorage.getItem('corona')
     coronaWins = JSON.parse(coronaWins)
-    return coronaWins.wins;
+    if (coronaWins) {
+      return coronaWins.wins;
+    } else {
+      return 0;
+    }
   }
 
   takeTurns() {
